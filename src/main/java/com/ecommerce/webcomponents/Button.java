dbaddc7b2b5dadde782e2f69e5d220c0780df0b6
Gajendra <gajendratm05@gmail.com>
2025-01-11 14:39:03 +0100

    feat: add webcomponents wrapper classes to encapsulate interactions with UI elements

---
+++ src/main/java/com/ecommerce/webcomponents/Button.java
@@ -0,0 +1,15 @@
+package com.ecommerce.webcomponents;
+
+public class Button {
+
+	/*
+	 * if we don't want to use WebElementUtils.java
+	 *  which contains centralized utility methods for
+	 *  buttons, dropdowns, inputfileds etc.
+	 * 
+	 * 
+	 * then this can be used as a individual  wrapper classes 
+	 * to encapsulate interactions with specific types of UI elements.
+	 *(like: click,isDisplayed,getText,isEnabled etc)
+	 * */
+}
---
+++ src/main/java/com/ecommerce/webcomponents/InputField.java
@@ -0,0 +1,15 @@
+package com.ecommerce.webcomponents;
+
+public class InputField {
+
+	/*
+	 * * if we don't want to use WebElementUtils.java
+	 *  which contains centralized utility methods for
+	 *  buttons, dropdowns, inputfileds etc.
+	 * 
+	 * this can be used as a wrapper classes 
+	 * to encapsulate interactions with specific types of UI elements.
+	 *(like: type,clear,getValue,sendKeys etc)
+	 * */
+	
+}

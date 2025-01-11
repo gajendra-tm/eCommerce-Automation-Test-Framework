4c0062d4d396833d963fda51ef7b382c6bb19155
Gajendra <gajendratm05@gmail.com>
2025-01-11 14:28:47 +0100

    feat: add browserdriver managers to handle specific browser driver settings individually

---
+++ src/main/java/com/ecommerce/browser/ChromeDriverManager.java
@@ -0,0 +1,13 @@
+package com.ecommerce.browser;
+
+public class ChromeDriverManager {
+
+	
+	
+	/* 
+	   * If there is a complex configuration or need to maintain 
+	   the browser-specific configuration and follow SRP, then
+	   we can use this ChromeDriverManager
+	*/
+	 
+}
---
+++ src/main/java/com/ecommerce/browser/FirefoxDriverManager.java
@@ -0,0 +1,9 @@
+package com.ecommerce.browser;
+
+public class FirefoxDriverManager {
+	/* 
+	   * If there is a complex configuration or need to maintain 
+	   the browser-specific configuration and follow SRP, then
+	   we can use this FirefoxDriverManager
+	*/
+}
